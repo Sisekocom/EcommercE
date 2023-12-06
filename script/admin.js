0f8d82e75170a3a14a6097a7b5a4cb9c4bb12274
Sisekocom <nonyongos1@gmail.com>
2023-12-06 16:27:50 +0200

    checkout

--- script/admin.js
+++ script/admin.js
@@ -1,1 +1,131 @@
-document.getElementById('CurrYear').textContent = new Date().getFullYear()
+// 
+let output = document.getElementById('output')
+// I must have an empty array  to add the products
+let myProducts = [];
+
+// list of products 
+let products = [
+
+    {
+        id: 1, 
+        name:'HP Mono LaserJet MFP 137fnw Printer' ,
+        amount: 5309.00 ,
+        image:'https://tech.co.za/wp-content/uploads/2022/02/4ZB84Aa-768x576.jpg'
+    },
+    {
+        id:2,
+        name:'Corsair TC200 Gaming Chair – Soft Fabric – ' ,
+        amount: 7759.00 ,
+        image:'https://tech.co.za/wp-content/uploads/2023/02/CF-9010049-WWa-768x576.png'
+       
+       
+    }
+    ,
+    {
+        id:3,
+        name:'Dell P2418HT Touch Monitor – 23.8″' ,
+        amount: 5819.00 ,
+        image:'https://tech.co.za/wp-content/uploads/2019/06/DELL-P2418HT-510x383.jpg'
+       
+       
+    }
+    ,
+    {
+        id:4,
+        name:'Asus Vivobook Pro 15 K6502ZC 15.6″ Laptop ' ,
+        amount: 24159.00,
+        image:'https://tech.co.za/wp-content/uploads/2023/07/Vivobook-Pro-15-K6502ZCa-768x576.png'
+       
+       
+    }
+    ,
+    {
+        id:5,
+        name:'Logitech MK330 Wireless Keyboard and Mouse' ,
+        amount: 699 ,
+        image:'https://www.firstshop.co.za/cdn/shop/products/920-003989-keyboards-35708394340516_700x.jpg?v=1673263740'
+       
+    },
+
+    
+]
+// this function will add the products to myProducts
+function displayResults(){
+    // this function will add the products to myProducts
+    myProducts.push(...products)
+
+    // console.log(myProducts);
+
+        myProducts.forEach(item => {
+
+            output.innerHTML += 
+            `
+            <div  class="card m-5" style="width: 17rem;">
+            <img src="${item.image} " class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${item.name}</h5>
+              <h4 class="card-text mt-5">R${item.amount}</h4>
+              <a href="#" class="btn btn-primary">Add</a>
+              <a href="#" class="btn btn-primary">Delete</a>
+            </div>
+          </div>
+            `
+        })
+
+        // 4-adding products to localStorage
+       
+        localStorage.setItem('myProducts', JSON.stringify(myProducts))
+
+    //must display them in the output
+}
+
+displayResults()
+
+
+function searchProducts(){
+    let sInput = document.querySelector('#Input');
+    let searchedProds = products.filter( prod => {
+        return prod.name.toLowerCase().includes( sInput.value )
+    });
+    output.innerHTML = ''
+    searchedProds.forEach(item => {
+
+        output.innerHTML += 
+        `
+        <div  class="card m-5" style="width: 17rem;">
+        <img src="${item.image} " class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <h4 class="card-text mt-5">${item.amount}</h4>
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+        `
+    })
+}
+
+
+function sortItems(){
+    let sortedItems = myProducts.sort((item1, item2) => {
+        return item2.amount - item1.amount;
+    })
+
+    console.log(sortedItems);
+    output.innerHTML = ''
+    sortedItems.forEach(ii=>{
+        output.innerHTML += 
+        `
+        <div  class="card m-5" style="width: 17rem;">
+        <img src="${ii.image} " class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${ii.name}</h5>
+          <h4 class="card-text mt-5">${ii.amount}</h4>
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+        `
+    })   
+}
+
+
+
--- script/checkout.js
+++ script/checkout.js
@@ -1,1 +1,48 @@
-document.getElementById('CurrYear').textContent = new Date().getFullYear()
+
+let cartSize = JSON.parse(localStorage.getItem('Checkout')).length || 0
+console.log(cartSize);
+
+document.querySelector('[cartSize]').textContent = cartSize;
+
+let List = JSON.parse(localStorage.getItem('checkout')) ?
+    JSON.parse(localStorage.getItem('checkout')) : [];
+let tbody = document.querySelector('tbody');
+(function displayCheckOut() {
+    try {
+        if (!List.length) throw "Please add the product to the checkout list.";
+        let groupBy = Object.groupBy(List, item => {
+            return item.id
+        })
+        let amountDue = 0;
+        for (let idx in groupBy) {
+            let totalAmount = groupBy[idx].length * groupBy[idx][0].amount;
+            amountDue += totalAmount;
+            tbody.innerHTML += `
+                <tr>
+                    <td>${groupBy[idx][0].name}</td>
+                    <td>${groupBy[idx][0].amount}</td>
+                    <td>${groupBy[idx].length}</td>
+                    <td>R${totalAmount}</td>
+                </tr>
+            `
+        }
+        // Display the amount due
+        tbody.innerHTML += `
+            <tr class="amount-due">
+                <td></td>                    
+                <td></td>                    
+                <td>Amount Due:</td> 
+                <td>R${amountDue}</td>
+            </tr>
+        `
+    } catch (e) {
+        tbody.innerText = e;
+        tbody.style = "font-weight: bold; font-size: 8rem;"
+    }
+})();
+
+let clearAll = document.querySelector('#clearAll');
+clearAll.addEventListener('click', () => {
+    localStorage.removeItem('checkout');
+    tbody.innerHTML = "Please add the product to the checkout list.";
+})
